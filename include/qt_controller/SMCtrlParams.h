271e376a1c7dd6203e4533872df1f046a48f4b49
wangzhao9562 <wangzhao9562@gmail.com>
2020-12-10 21:44:44 -0800

    Update slide controller paramaters definition

--- include/qt_controller/SMCtrlParams.h
+++ include/qt_controller/SMCtrlParams.h
@@ -1,26 +1,34 @@
-#ifndef SMCTRLPARAMS_H_
-#define SMCTRLPARAMS_H_
+/*
+ * Filename: SMCtrlParams.h
+ * Path: auv_controller
+ * Created Date: Tuesday, September 8th 2020, 10:32:15 pm
+ * Author: zhao wang
+ * 
+ * Copyright (c) 2020 Your Company
+ */
+
+#ifndef SM_CTRL_PARAMS_H_
+#define SM_CTRL_PARAMS_H_
+
+#include <stdio.h>
 
 namespace qt_controller{
-
-/**
- * @brief Slide model control parameters
- */ 
-struct SMCtrlParams
-{
-    double c_, k_, alpha_;
-    double boundary_thick_;
-
     /**
-     * @brief Set control parameters
-     */ 
-    void setCtrlParams(double c, double k, double alpha, double bt){
-        c_ = c;
-        k_ = k;
-        alpha_ = alpha;
-        boundary_thick_ = bt;
-    }
-}; // SMCtrlParams
+     * @brief Slide control parameters
+     */
+    struct SMCtrlParams{
+        double e_;
+        double dot_e_;
+        double s_;
+        double l_;
+
+        /**
+         * @brief Print slide control parameters
+         */ 
+        void printSMCtrlParameters()const{
+            printf("SMCtrlParam:{e:%f dot_e:%f s:%f l:%f}\n", e_, dot_e_, s_, l_);
+        }
+    }; // SMCtrlParams
 }; // ns
 
 #endif
