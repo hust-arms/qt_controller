84f426220345b44662cd4c24a01e63de3c153a25
wangzhao9562 <wangzhao9562@gmail.com>
2020-12-29 05:02:58 -0800

    Change default parameters

--- src/QTControllerROS.cpp
+++ src/QTControllerROS.cpp
@@ -29,7 +29,7 @@ namespace qt_controller{
 
         // Default parameters
         private_nh.param("base_frame", base_frame_, std::string("/base_link"));
-        private_nh.param("rpm", rpm_, 1500);
+        private_nh.param("rpm", rpm_, 2500);
         private_nh.param("control_period", dt_, 0.1);
         private_nh.param("xd", x_d_, 30.0);
         private_nh.param("yd", y_d_, 0.0);
