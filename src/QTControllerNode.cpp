1bcd1f89d854a657c9d178126d8bbd5cf1afaecf
wangzhao9562 <wangzhao9562@gmail.com>
2021-01-08 17:45:52 -0800

    Add command line parser for qt controller node

--- src/QTControllerNode.cpp
+++ src/QTControllerNode.cpp
@@ -6,14 +6,24 @@
  * 
  * Copyright (c) 2020 Your Company
  */
-
+#include "stdio.h"
 #include "qt_controller/QTControllerROS.h"
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "qt_controller_node");
 
+    int ctrl_flag = 0; // 0 - slide model controller 1 - PID controller
+
+    if(argc == 2 && strcmp(argv[1], "pid") == 0){
+        printf("select controller: %s\n", argv[1]);
+        ctrl_flag = 1;
+    }
+    else{
+        printf("select controller: slide model\n");
+    }
+
     std::string auv_ns = "uvsm";
-    qt_controller::QTControllerROS QT_controller(auv_ns);
+    qt_controller::QTControllerROS QT_controller(auv_ns, ctrl_flag);
 
     ros::spin();
 
